e8b8d8a3ed4005b9e41a550c3164f73df0d98278
5690 <5690Programmers@gmail.com>
2018-01-11 08:35:59 -0600

    Added AHRS
    Test AHRS on T.O.M

--- 2018Code/src/Robot.cpp
+++ 2018Code/src/Robot.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <memory>
 #include <string>
-
-
 #include <XboxController.h>
 #include <CameraServer.h>
-#include <ADXRS450_Gyro.h>
-#include <ADXL362.h>
-
+#include <AHRS.h>
 #include <Joystick.h>
 #include <SampleRobot.h>
 #include <SmartDashboard/SendableChooser.h>
@@ -16,51 +12,42 @@
 #include <Timer.h>
 #include <PWMTalonSRX.h>
 #include <SpeedControllerGroup.h>
-/**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will
- * automatically call your Autonomous and OperatorControl methods at the right
- * time as controlled by the switches on the driver station or the field
- * controls.
- *
- * WARNING: While it may look like a good choice to use for your code if you're
- * inexperienced, don't. Unless you know what you are doing, complex code will
- * be much more difficult under this system. Use IterativeRobot or Command-Based
- * instead if you're new.
- */
+#include <VictorSP.h>
+
+const static double kP = 0.03f;
+const static double kI = 0.00f;
+const static double kD = 0.00f;
+const static double kF = 0.00f;
+const static double kToleranceDegrees = 2.0f;
+
+
+
 class Robot: public frc::SampleRobot {
-
-
-
-
 	//Controllers
 	frc::XboxController Xbox{1};
 	frc::Joystick Stick {0};
 
 	//Add-ons
-	frc::ADXRS450_Gyro Gyro;
+	AHRS *ahrs
+	double rotateToAngleRate;
+	PIDController *turnController
 
 	//Motors
-	frc::PWMTalonSRX Arm{4};
+	/*
+	frc::VictorSP Arm{4};
 	frc::PWMTalonSRX Intake_one{5}
 	frc::PWMTalonSRX Intake_two{6}
-
 	frc::SpeedControllerGroup Intake{Intake_one, Intake_two};
+	*/
+	frc::VictorSP front_left {1};
+	frc::VictorSP front_right {0};
 
-
-	frc::PWMTalonSRX front_left {0};
-	frc::PWMTalonSRX back_left {1};
-	frc::PWMTalonSRX front_right {3};
-	frc::PWMTalonSRX back_right {2};
-
-	frc::SpeedControllerGroup left{front_left, back_left};
-	frc::SpeedControllerGroup right{front_right, back_right};
-
+	frc::SpeedControllerGroup left{front_left};
+	frc::SpeedControllerGroup right{front_right};
 	frc::DifferentialDrive myRobot {right, left};
-	//Dashboard Choosers
+
 	frc::SendableChooser<std::string> side;
 	frc::SendableChooser<std::string> start;
-	//Dashboard Options
 	const std::string red = "Red";
 	const std::string blue = "Blue";
 	const std::string basic = "Go Forward";
@@ -69,8 +56,30 @@ class Robot: public frc::SampleRobot {
 	const std::string startleft = "Left Start";
 
 public:
+
+	virtual void PIDWrite(double output) {
+	        this->rotateToAngleRate = output;
+
 	Robot() {
 		myRobot.SetExpiration(0.1);
+
+		 rotateToAngleRate = 0.0f;
+		 robotDrive.SetExpiration(0.1);
+		 	 try {
+		 		 ahrs = new AHRS(SPI::Port::kMXP);
+		 	 } catch (std::exception& ex ) {
+		        std::string err_string = "Error instantiating navX MXP:  ";
+		        err_string += ex.what();
+		        DriverStation::ReportError(err_string.c_str());
+		     }
+		        turnController = new PIDController(kP, kI, kD, kF, ahrs, this);
+		        turnController->SetInputRange(-180.0f,  180.0f);
+		        turnController->SetOutputRange(-1.0, 1.0);
+		        turnController->SetAbsoluteTolerance(kToleranceDegrees);
+		        turnController->SetContinuous(true);
+		    }
+
+
 	}
 
 	void RobotInit() {
@@ -97,6 +106,14 @@ public:
 
 		if ((Start == startleft) && (Color == blue)) {
 			//make auto for left start blue
+			//Example to test AHRS.
+			AHRS.Reset();
+			myRobot.drive(0.5,0.5);
+			while(IsAutonomous() && (AHRS.GetAngle() <= 90)){
+				frc::SmartDashboard:PutNumber("Angle", AHRS.GetAngle());
+				myRobot.Drive(-0.25, 0.8);
+			}
+			myRobot.Drive(0,0);
 
 		} else if ((Start == startmid) && (Color == blue)) {
 			//make auto for mid start blue
@@ -131,7 +148,29 @@ public:
 			myRobot.TankDrive(Xbox.GetY(), Xbox.GetX()); //Not sure this works. Test this eventually/
 
 
+            bool reset_yaw_button_pressed = Xbox.GetStartButton();
+            if ( reset_yaw_button_pressed ) {
+                ahrs->ZeroYaw();
+            }
+            bool rotateToAngle = false;
+            if (Xbox.GetYButton()) {
+                turnController->SetSetpoint(0.0f);
+                rotateToAngle = true;
+            } else if (Xbox.GetBButton()) {
+                turnController->SetSetpoint(90.0f);
+                rotateToAngle = true;
+            } else if (Xbox.GetAButton()) {
+                turnController->SetSetpoint(179.9f);
+                rotateToAngle = true;
+            } else if (Xbox.GetXButton()) {
+                turnController->SetSetpoint(-90.0f);
+                rotateToAngle = true;
+            }
 
+
+
+
+            /*
 			if (Xbox.GetTriggerAxis(XboxController::JoystickHand(0))) //0 is left trigger
 			{
 				Intake.Set(-0.5);
@@ -145,7 +184,7 @@ public:
 				Intake.Set(0)
 			}
 
-
+			/*
 			//Move elevator arm up(X) and down(B)
 			if(Xbox.GetXButton())
 			{
@@ -165,6 +204,8 @@ public:
 				Arm.Set(0):
 			}
 
+			*/
+
 			frc::Wait(0.005);
 		}
 	}
@@ -174,4 +215,4 @@ public:
 	}
 };
 
-START_ROBOT_CLASS(Robot)
+START_ROBOT_CLASS(Robot);
